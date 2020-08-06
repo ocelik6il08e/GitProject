e96b596309f09e75f0da63d7f5cef8bc2a414a50
ocelik66il08e <usaonurcelik@gmail.com>
2020-08-05 20:55:33 -0400

    GitFun.java

---
+++ src/utility/GitFun.java
@@ -0,0 +1,11 @@
+package utility;
+
+public class GitFun {
+    public static void main(String[] args) {
+
+        String name = "Onur";
+        String lastname = "Celik";
+        System.out.println(name);
+        System.out.println(lastname);
+    }
+}
